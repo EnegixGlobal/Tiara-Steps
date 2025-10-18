10948bb8809e07befaab54e158e6ce8a3a06b5d6
Mustak Ansary <mustak65ee@gmail.com>
2025-10-18 13:50:22 +0530

    changes in shoe svg

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,19 +1,29 @@
-import image3 from "../Images/shoes/image 3.svg";
-import image1 from "../Images/shoes/image 1.svg";
-import image2 from "../Images/shoes/image 2.svg";
-import image4 from "../Images/shoes/image 4.svg";
+import image3 from "../Images/shoe1.svg";
+import image1 from "../Images/shoe2.svg";
+import image2 from "../Images/shoe3.svg";
+import image4 from "../Images/shoe4.svg";
+import image5 from "../Images/shoe5.svg";
 import React, { useState } from "react";
 
 const Container = () => {
   const [activeBox, setActiveBox] = useState(null);
 
+  // const boxes = [
+  //   { id: 1, image: image4, color: "#e6e2d9" },
+  //   { id: 2, image: image2, color: "#64aedd" },
+  //   { id: 3, image: image1, color: "#42a3c7" },
+  //   { id: 4, image: image2, color: "#295eb4" },
+  //   { id: 5, image: image3, color: "#08214a" },
+  // ];
+
   const boxes = [
-    { id: 1, image: image4, color: "#e6e2d9" },
-    { id: 2, image: image2, color: "#64aedd" },
-    { id: 3, image: image1, color: "#42a3c7" },
-    { id: 4, image: image2, color: "#295eb4" },
-    { id: 5, image: image3, color: "#08214a" },
+    { id: 1, image: image1, color: "#FFC5D3" }, // pastel pink
+    { id: 2, image: image2, color: "#FFB6A0" }, // peach coral
+    { id: 3, image: image3, color: "#C8A2C8" }, // lavender
+    { id: 4, image: image4, color: "#F7D794" }, // soft gold
+    { id: 5, image: image5, color: "#FF8DA1" }, // rose pink
   ];
+
 
   const handleMouseEnter = (id) => {
     setActiveBox(id);
